af9bef5a6e87453320c5b9d21878db9bd6888ccb
Ankit Gadling <ankitgadling2@gmail.com>
2024-03-19 21:43:27 +0530

    changed media path

--- charaksamhita/settings.py
+++ charaksamhita/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'  # URL for serving media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Local filesystem path where media files will be stored
+
+
--- charaksamhita/urls.py
+++ charaksamhita/urls.py
@@ -24,7 +24,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+# urlpatterns = [
+#     # Other URL patterns
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('search_disease.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
