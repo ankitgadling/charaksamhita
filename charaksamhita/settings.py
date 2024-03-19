bcd6a0e6fc751fd47883e7c535b7d63ff2ade650
Ankit Gadling <ankitgadling2@gmail.com>
2024-03-19 20:41:41 +0530

    modifed the templates path

--- charaksamhita/settings.py
+++ charaksamhita/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'charaksamhita.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['charaksamhita/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
